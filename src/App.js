44590070ddb11770f36dbda543260f62c454f4cf
Naheed <>
2022-09-22 10:10:40 +0400

    updated screens and bg color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a react weapp to demonstrate CI / CD pipelines
         </p>
         <a
           className="App-link"
